15101f501349f5e9bb022b9b0bd3d8e59fc1d132
杨俊康 <903414363@qq.com>
2021-07-22 16:26:39 +0800

    析构器实践

--- 基础教程/基础教程.playground/Sources/Part6.swift
+++ 基础教程/基础教程.playground/Sources/Part6.swift
@@ -83,3 +83,59 @@ class ShoppingListItem: RecipeIngredient {
         return output
     }
 }
+
+// 可失败构造器
+class Product {
+    let name: String
+    init?(name: String) {
+        if name.isEmpty { return nil }
+        self.name = name
+    }
+}
+
+class CartItem: Product {
+    let quantity: Int
+    init?(name: String, quantity: Int) {
+        if quantity < 1 { return nil }
+        self.quantity = quantity
+        super.init(name: name)
+    }
+}
+
+// 析构器实践
+class Bank {
+    static var coinsInBank = 10_000
+    static func distribute(coins numberOfCoinsRequested: Int) -> Int {
+        let numberOfCoinsToVend = min(numberOfCoinsRequested, coinsInBank)
+        coinsInBank -= numberOfCoinsToVend
+        return numberOfCoinsToVend
+    }
+    static func receive(coins: Int) {
+        coinsInBank += coins
+    }
+}
+
+class Player {
+    var coinsInPurse: Int
+    init(coins: Int) {
+        self.coinsInPurse = Bank.distribute(coins: coins)
+    }
+    func win(coins: Int) {
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    deinit {
+        Bank.receive(coins: coinsInPurse)
+    }
+}
+
+public func testDeinit() {
+    var playerOne: Player? = Player(coins: 100)
+    print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
+    print("The bank now has \(Bank.coinsInBank) coins")
+    playerOne?.win(coins: 2_000)
+    print("PlayerOne won 2000 coins & now has \(playerOne!.coinsInPurse) coins")
+    print("The bank now has \(Bank.coinsInBank) coins")
+    playerOne = nil
+    print("PlayerOne has left the game")
+    print("The bank now has \(Bank.coinsInBank) coins")
+}
