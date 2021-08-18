a3c5ab1a8763521b4e73f6368edb2e6c00d52630
杨俊康 <903414363@qq.com>
2021-08-18 17:17:02 +0800

    commit

--- 基础教程/基础教程.playground/Contents.swift
+++ 基础教程/基础教程.playground/Contents.swift
@@ -4,4 +4,13 @@ var greeting = "Hello, playground"
 
 let actor = Actor(name: "guluwa")
 
-test9()
+//test9()
+
+intIntoString(number: 1)
+
+func intIntoString(number: Int) -> String{
+    let formatter = NumberFormatter()
+    formatter.numberStyle = NumberFormatter.Style(rawValue: UInt(CFNumberFormatterRoundingMode.roundHalfDown.rawValue))!
+    let string:String = formatter.string(from: NSNumber(value: number)) ?? ""
+    return string
+}
